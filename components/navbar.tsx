3aa2fa2bad51743929710ea231cda6b2a93870ce
VasuDevrani <vasu2002devrani@gmail.com>
2023-08-01 20:57:30 +0530

    nav and sidebar

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Menu, Sparkles } from "lucide-react";
+import { ModeToggle } from "./theme-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            ai.mate
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button size="sm" variant="premium">
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
+        </Button>
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
