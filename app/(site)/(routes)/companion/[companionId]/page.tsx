3ef8b65552a3316ccba5f5561a4f408fcb4f8c81
VasuDevrani <vasu2002devrani@gmail.com>
2023-08-02 10:27:45 +0530

    companion creation form ui cloudinary image upload setup

---
+++ app/(site)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,22 @@
+import { CompanionForm } from "@/components/companion-form";
+import prismadb from "@/lib/prismadb";
+
+interface CompanionPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionPage = async ({ params }: CompanionPageProps) => {
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+  const categories = await prismadb.category.findMany();
+  return (
+    <CompanionForm initialData={companion} categories={categories}/>
+  );
+};
+
+export default CompanionPage;
