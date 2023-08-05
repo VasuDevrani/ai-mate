826249f9473e3fd5a2897e77dfae50eb77942def
VasuDevrani <vasu2002devrani@gmail.com>
2023-08-05 16:25:51 +0530

    bug fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -11,14 +11,14 @@ import {
 import { Sidebar } from "@/components/sidebar";
 import { Menu } from "lucide-react";
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar/>
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
